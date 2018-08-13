669990997e364fa6c0411ac0696ef5335c33c3e9
MaxAAustin <maxaustin92@gmail.com>
2018-08-13 01:48:54 -0400

    Fleshed out basic client/ server for java

--- java/src/proxy/client.java
+++ java/src/proxy/client.java
@@ -5,18 +5,54 @@ package proxy;
 import java.net.*;
 import java.io.*;
 
-public class EClient{
+public class EClient {
 
-    public EClient(String host, int port){
+    public EClient(String host, int port) {
         try {
-            String serverHostName = new String ("127.0.0.1");
+            String serverHostName = new String("127.0.0.1");
+            System.out.print("Connecting to host " + serverHostName + " on port " + port);
+
+            Socket echoSocket = null;
+            PrintWriter out = null;
+            BufferedReader in = null;
+
+            try {
+                echoSocket = new Socket(serverHostName, 8081);
+                out = new PrintWriter(echoSocket.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+            } catch (UnknownHostException e) {
+                System.err.println("Unknown host: " + serverHostName);
+                System.exit(1);
+            } catch (IOException e) {
+                System.err.println("Unable to get streams from server");
+                System.exit(1);
+            }
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+
+            while (true) {
+                System.out.print("Client: ");
+                String userInput = stdIn.readLine();
+
+                if ("q".equalsIgnoreCase(userInput)) {
+                    break;
+                }
+                out.println(userInput);
+                System.out.println("server: " + in.readLine());
+            }
+
+            out.close();
+            in.close();
+            stdIn.close();
+            echoSocket.close();
         } catch (Exception e) {
-            //TODO: handle exception
+            e.printStackTrace();
         }
+
     }
+
     public static void main(String[] args) {
         String host = "127.0.0.1";
         int port = 8081;
-        new EClient(host, port); 
+        new EClient(host, port);
     }
 }
